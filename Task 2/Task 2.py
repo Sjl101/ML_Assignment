c42b01a80d4c8791e7232ac18c837c46051bfb3c
Sjl101 <shanejordanlawson@sky.com>
2022-02-16 15:41:24 +0000

    .

--- Task 2/Task 2.py
+++ Task 2/Task 2.py
@@ -14,19 +14,19 @@ def compute_euclidean_distance(vec_1, vec_2):
     return distance
 
 def initialise_centroids(dataset, k):
-    dt = dataset.values
+    dt = dataset.to_numpy()
     np.random.shuffle(dt)
     centroids = dt[:k, :]
     return centroids
 
 def kmeans(dataset, k):
-    data_m = dataset.values
+    data_m = dataset.to_numpy()
     cluster_assigned = np.zeros((len(data_m), 2))
-    centroids = initialise_centroids(dataset=dataset, k=k)
+    centroids = initialise_centroids(dataset, k)
     for v_index, vector in enumerate(data_m):
         centroid_distances = np.zeros(len(centroids))
         for centroid_i, centroid in enumerate(centroids):
-            distance = compute_euclidean_distance(vec_1=vector, vec_2=centroid)
+            distance = compute_euclidean_distance(vector, centroid)
             centroid_distances[centroid_i] = distance  
         min_distance = np.min(centroid_distances)
         assigned_cluster = np.argmin(centroid_distances)
@@ -47,38 +47,26 @@ def kmeans(dataset, k):
         
     return centroids, new_centroids, cluster_assigned_df
 
-centroids, mean_centroids, clusassdf = kmeans(data, k=3)
+centroids, mean_cent, clusassdf = kmeans(data, k=3)
 print('The centroids: ', centroids)
-print('The mean centroids: ', mean_centroids)
+print('The mean centroids: ', mean_cent)
 
-# Time to plot the graphs:
 km1 = clusassdf[clusassdf['assigned_centroid'] == 0]
 km2 = clusassdf[clusassdf['assigned_centroid'] == 1]
 km3 = clusassdf[clusassdf['assigned_centroid'] == 2]
-# PLot the graph:
-# Plot the first group:
 plt.scatter(km1['height'],km1['tail length'],color='blue')
 plt.scatter(km2['height'],km2['tail length'],color='red')
 plt.scatter(km3['height'],km3['tail length'],color='purple')
-# Plot the first centroids height = [0] tail length = [1]:
 plt.scatter(centroids[0, 0],centroids[0, 1],color='black')
 plt.scatter(centroids[1, 0],centroids[1, 1],color='black')
-# Plot the first mean centroid:
-plt.scatter(mean_centroids[0, 0],mean_centroids[1, 1],color='yellow')
-# Plot the mean centroids:
-plt.scatter(mean_centroids[1, 0],mean_centroids[1, 1,],color='yellow')
+plt.scatter(mean_cent[0, 0],mean_cent[1, 1],color='yellow')
+plt.scatter(mean_cent[1, 0],mean_cent[1, 1,],color='yellow')
 plt.show()
-# b) The second plot is also a scatter plot, where x axis is the "height" fearture and y axis is the "leg length" feature; 
-#    As before use different colours to depict the data points from different clusters.
 plt.scatter(km1['height'],km1['leg length'],color='cyan')
 plt.scatter(km2['height'],km2['leg length'],color='lime')
 plt.scatter(km3['height'],km3['leg length'],color='magenta')
-# Plot the first centroids height = [0] tail length = [1]:
 plt.scatter(centroids[0, 0],centroids[0, 2],color='black')
-# Plot the second centroid:
 plt.scatter(centroids[1, 0],centroids[1, 2],color='black')
-# Plot the first mean centroid:
-plt.scatter(mean_centroids[0, 0],mean_centroids[1, 2],color='yellow')
-# Plot the mean centroids:
-plt.scatter(mean_centroids[1, 0],mean_centroids[1, 2],color='yellow')
+plt.scatter(mean_cent[0, 0],mean_cent[1, 2],color='yellow')
+plt.scatter(mean_cent[1, 0],mean_cent[1, 2],color='yellow')
 plt.show()
